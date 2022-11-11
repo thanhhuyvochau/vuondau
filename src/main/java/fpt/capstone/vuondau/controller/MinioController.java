3d8a152545edd015f5432c9feadfed2f9c5df701
dangnlh <dangnlh@hiworld.com.vn>
2022-11-11 17:54:05 +0700

    course API  done

---
+++ src/main/java/fpt/capstone/vuondau/controller/MinioController.java
@@ -0,0 +1,19 @@
+package fpt.capstone.vuondau.controller;
+
+import fpt.capstone.vuondau.entity.Account;
+import fpt.capstone.vuondau.entity.common.ApiResponse;
+import fpt.capstone.vuondau.entity.request.AccountExistedTeacherRequest;
+import fpt.capstone.vuondau.entity.response.AccountTeacherResponse;
+import fpt.capstone.vuondau.service.IAccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/files")
+public class MinioController {
+
+
+}
