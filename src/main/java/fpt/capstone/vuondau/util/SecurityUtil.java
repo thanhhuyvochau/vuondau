824c886237dd6bf85ac7974db95fd0358837d166
Thanh Huy <huyvct@hiworld.com.vn>
2022-11-14 11:43:34 +0700

    git done question function

---
+++ src/main/java/fpt/capstone/vuondau/util/SecurityUtil.java
@@ -0,0 +1,30 @@
+package fpt.capstone.vuondau.util;
+
+import fpt.capstone.vuondau.entity.Account;
+import fpt.capstone.vuondau.entity.common.ApiException;
+import fpt.capstone.vuondau.repository.AccountRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class SecurityUtil {
+
+    private final AccountRepository accountRepository;
+
+    public SecurityUtil(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Jwt principal = (Jwt) authentication.getPrincipal();
+        String username = principal.getClaimAsString("user_name");
+        return Optional.ofNullable(accountRepository.findByUsername(username))
+                .orElseThrow(() -> ApiException.create(HttpStatus.NOT_FOUND).withMessage("Student not found by username"));
+    }
+}
