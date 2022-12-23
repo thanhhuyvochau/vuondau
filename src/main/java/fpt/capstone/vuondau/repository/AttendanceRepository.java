b213e452c3d8fcada09e7c3b2c6229a96b94b4ac
dangnlh <dangnlh@hiworld.com.vn>
2022-12-23 07:32:37 +0700

    pull dev

---
+++ src/main/java/fpt/capstone/vuondau/repository/AttendanceRepository.java
@@ -0,0 +1,18 @@
+package fpt.capstone.vuondau.repository;
+
+import fpt.capstone.vuondau.entity.*;
+import fpt.capstone.vuondau.entity.Class;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
+
+    List<Attendance> findAllByStudentClassKeyId(StudentClassKey studentClassKey ) ;
+    List<Attendance> findAllByTimeTableIn(List<TimeTable>timeTables  ) ;
+
+}
