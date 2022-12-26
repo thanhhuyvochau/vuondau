6ad3be4277ef6d0399de6d43a6e063f2533224a3
Thanh Huy <huyvct@hiworld.com.vn>
2022-12-26 22:52:05 +0700

    resovle conflic

--- src/main/java/fpt/capstone/vuondau/controller/AccountController.java
+++ src/main/java/fpt/capstone/vuondau/controller/AccountController.java
@@ -12,9 +12,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.security.Principal;
 
 @RestController
 @RequestMapping("api/accounts")
