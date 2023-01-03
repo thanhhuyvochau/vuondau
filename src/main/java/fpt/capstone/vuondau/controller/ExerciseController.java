12d2df0795cf6771e77b4294985445c036665026
dangnlh <dangnlh@hiworld.com.vn>
2023-01-03 16:45:13 +0700

    fix api create class

--- src/main/java/fpt/capstone/vuondau/controller/ExerciseController.java
+++ src/main/java/fpt/capstone/vuondau/controller/ExerciseController.java
@@ -1,33 +1,35 @@
 package fpt.capstone.vuondau.controller;
 
-import fpt.capstone.vuondau.entity.common.ApiPage;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import fpt.capstone.vuondau.entity.common.ApiResponse;
-import fpt.capstone.vuondau.entity.dto.ClassExerciseDto;
-import fpt.capstone.vuondau.entity.dto.FeedBackDto;
+import fpt.capstone.vuondau.entity.response.ClassResourcesResponse;
+import fpt.capstone.vuondau.moodle.response.MoodleRecourseDtoResponse;
 import fpt.capstone.vuondau.service.IExerciseService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+import java.util.List;
 
 @RestController
-@RequestMapping("api/exercise")
+@RequestMapping("/api/exercise")
 public class ExerciseController {
 
-    private final IExerciseService iExerciseService ;
+private final IExerciseService iExerciseService ;
 
     public ExerciseController(IExerciseService iExerciseService) {
         this.iExerciseService = iExerciseService;
     }
 
-    @Operation(summary = "Lấy bài tập theo lớp ")
-    @GetMapping("/class/exercises")
-    @PreAuthorize("hasAnyAuthority('STUDENT','TEACHER')")
-    public ResponseEntity<ApiResponse<ApiPage<ClassExerciseDto>>> getExerciseOfLClass(Pageable pageable ) {
-        return ResponseEntity.ok(ApiResponse.success(iExerciseService.getExerciseOfLClass(pageable)));
+    @Operation(summary = "hoc sinh xem chi tiết resource (bai tập) của lớp")
+    @GetMapping("{classId}")
+    @PreAuthorize("hasAuthority('STUDENT')")
+    public ResponseEntity<ApiResponse<List<MoodleRecourseDtoResponse>>> getExerciseInClass(@PathVariable Long classId) throws JsonProcessingException {
+        return ResponseEntity.ok(ApiResponse.success(iExerciseService.getExerciseInClass(classId)));
     }
 
 
