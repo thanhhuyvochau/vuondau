d85d13453071adbf1ece527dfdf6d02d2b68e426
dangnlh <dangnlh@hiworld.com.vn>
2023-01-23 10:33:22 +0700

    fix api admin duyetj lop

--- src/main/java/fpt/capstone/vuondau/service/ITeacherService.java
+++ src/main/java/fpt/capstone/vuondau/service/ITeacherService.java
@@ -2,11 +2,16 @@ package fpt.capstone.vuondau.service;
 
 import fpt.capstone.vuondau.entity.Account;
 import fpt.capstone.vuondau.entity.Role;
+import fpt.capstone.vuondau.entity.common.ApiPage;
 import fpt.capstone.vuondau.entity.common.EAccountRole;
+import fpt.capstone.vuondau.entity.response.AccountDetailResponse;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ITeacherService {
     List<Account> getTeacher();
+
+    ApiPage<AccountDetailResponse> getAllTeacher(Pageable pageable);
 
 }
